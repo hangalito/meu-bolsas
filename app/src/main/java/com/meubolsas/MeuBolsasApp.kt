8180b3c36d646ff094a668b6732ff418fc2e8744
Bartolomeu Hangalo <bartolomeuhangalo1@gmail.com>
2023-12-12 03:43:28 +0100

    Top App Bar colors changed

--- app/src/main/java/com/meubolsas/MeuBolsasApp.kt
+++ app/src/main/java/com/meubolsas/MeuBolsasApp.kt
@@ -15,10 +15,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledIconToggleButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -59,7 +61,11 @@ fun MeuBolsasApp(
                         modifier = Modifier.padding(all = 8.dp)
                     )
                 },
-                modifier = Modifier.shadow(elevation = 6.dp)
+                modifier = Modifier.shadow(elevation = 6.dp),
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorScheme.primary,
+                    titleContentColor = colorScheme.onPrimary,
+                ),
             )
         },
         bottomBar = {
